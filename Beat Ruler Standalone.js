e77d818027bfc16b8a7ec4100d0868721155f533
KirisuNJSC <kirisunjsc@gmail.com>
2023-03-04 10:21:49 +0700

    naming error, renamed eighths to sixteenths

--- Beat Ruler Standalone.js
+++ Beat Ruler Standalone.js
@@ -8,13 +8,13 @@ mainTime2 = a - time - thisComp.frameDuration;
 if (mainTime >= 0) {
     mainBeats = Math.floor((mainTime * bpm * 16) / 60);
     quarters = Math.floor(((mainBeats / 16) % 4) + 1);
-    eighths = Math.floor((mainBeats / 4) % 4) + 1;
+    sixteenths = Math.floor((mainBeats / 4) % 4) + 1;
     measures = Math.floor(mainBeats / 16 / 4) + 1;
 } else {
     mainBeats = Math.floor((mainTime2 * bpm * 16) / 60);
     quarters = 4 - Math.floor((mainBeats / 16) % 4);
-    eighths = 4 - Math.floor((mainBeats / 4) % 4);
+    sixteenths = 4 - Math.floor((mainBeats / 4) % 4);
     measures = -1 * (Math.floor(mainBeats / 16 / 4) + 1);
 }
 
-result = measures + ":" + quarters + ":" + eighths;
+result = measures + ":" + quarters + ":" + sixteenths;
